b7c823b8eadba3812ff3d781879035a98d77621a
Datodia <dato.diasamidze.02@gmail.com>
2022-10-29 22:02:57 +0400

    feat:add functinality

--- main.js
+++ main.js
@@ -2,8 +2,35 @@
 const slider = document.getElementById('slider');
 const selector = document.querySelector('#selector .selectBtn');
 const progres = document.querySelector('#progressBar');
+const price = document.querySelector('#price');
+const btn = document.querySelector('#test');
+
+
+let state = false;
+btn.addEventListener('click', () => {
+    state = !state
+    if (state) {
+        price.innerHTML = `$ ${slider.value * 12 - (slider.value * 3)}.00`
+    } else {
+        price.innerHTML = `$ ${slider.value}.00`
+    }
+})
+
 
 slider.oninput = function () {
     selector.style.left = (this.value / 32) * 680 + "%"
     progres.style.width = (this.value / 32) * 100 + "%"
+    if (state) {
+        price.innerHTML = `$ ${slider.value * 12 - (slider.value * 3)}.00`
+    } else {
+        price.innerHTML = `$ ${slider.value}.00`
+    }
 }
+
+
+
+
+
+
+
+
